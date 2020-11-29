7c805765dc3e1db8c0180351db55b687dc152aeb
jessicarf18 <jessicarosalia678@gmail.com>
2020-11-29 14:03:27 -0300

    Montando estrutura de pastas e criando primeira tela do crud Animal

---
+++ src/br/com/farmcontrol/Main.java
@@ -0,0 +1,28 @@
+package br.com.farmcontrol;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/br/com/farmcontrol/view/resources/FXML/CrudAnimal.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add("/br/com/farmcontrol/view/resources/styles/fxstyle.css");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/br/com/farmcontrol/controller/CrudAnimalController.java
@@ -0,0 +1,161 @@
+package br.com.farmcontrol.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import javafx.fxml.Initializable;
+
+public class CrudAnimalController implements Initializable {
+
+	 @FXML
+	    private AnchorPane tdo;
+
+	    @FXML
+	    private AnchorPane menu;
+
+	    @FXML
+	    private Button pagina_inicial;
+
+	    @FXML
+	    private ImageView img_paginainicial;
+
+	    @FXML
+	    private Button animal;
+
+	    @FXML
+	    private ImageView img_animal;
+
+	    @FXML
+	    private Button vacina;
+
+	    @FXML
+	    private ImageView img_vacina;
+
+	    @FXML
+	    private Button leite;
+
+	    @FXML
+	    private ImageView img_leite;
+
+	    @FXML
+	    private Button ovos;
+
+	    @FXML
+	    private ImageView img_ovo;
+
+	    @FXML
+	    private Button racao;
+
+	    @FXML
+	    private ImageView img_racao;
+
+	    @FXML
+	    private Button reproducao;
+
+	    @FXML
+	    private ImageView img_reproducao;
+
+	    @FXML
+	    private Button relatorio;
+
+	    @FXML
+	    private ImageView img_relatorio;
+
+	    @FXML
+	    private ImageView logo;
+
+	    @FXML
+	    private AnchorPane miolo;
+
+	    @FXML
+	    private Button cadastrar_animal;
+
+	    @FXML
+	    private Button editar_animal;
+
+	    @FXML
+	    private Button buscar_animal;
+
+	    @FXML
+	    private Button excluir_animal;
+
+	    @FXML
+	    void buscarAnimal(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void cadastrarAnimal(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void editarAnimal(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void excluirAnimal(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void exibirPaginaAnimais(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void exibirPaginaInicial(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void exibirPaginaLeite(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void exibirPaginaOvos(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void exibirPaginaRacao(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void exibirPaginaRelatorios(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void exibirPaginaReproducao(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void exibirPaginaVacina(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void mudarCor(MouseEvent event) {
+
+	    }
+
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
